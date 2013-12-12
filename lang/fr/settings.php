063c1a1af4910776fff0f54c061b4f0c84c0db75
bruno <bruno@ninesys.fr>
2013-12-12 23:15:28 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author bruno <bruno@ninesys.fr>
+ */
+$lang['404page']               = 'Erreur 404 : page introuvable';
