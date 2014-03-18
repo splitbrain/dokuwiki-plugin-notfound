bc1a5be6745a473aa8b0d393b0f8aea32cc82f33
Rene <wllywlnt@yahoo.com>
2014-03-18 15:30:15 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['404page']               = 'De pagina die getoond wordt bij een 404';
