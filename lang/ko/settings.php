27cb38358393e192fce2f6fb1edc9dc6162ffd65
Myeongjin <aranet100@gmail.com>
2016-07-27 13:10:22 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Myeongjin <aranet100@gmail.com>
  */
-$lang['404page']               = '404에 보여줄 페이지';
+$lang['404page']               = '404에 표시할 페이지';
