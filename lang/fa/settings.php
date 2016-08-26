fd8a6089c0da171c1a5a841098534783ec5b11ab
Sam01 <m.sajad079@gmail.com>
2016-08-26 14:50:43 +0200

    translation update

---
+++ lang/fa/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['404page']               = 'صفحه‌ای برای نمایش روی یک 404';
