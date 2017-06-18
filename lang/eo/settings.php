3621374e316885b97e08de995d75ed7a5fe33b7d
Moisés Braga Ribeiro <moisesbr@gmail.com>
2017-06-18 22:20:30 +0200

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Moisés Braga Ribeiro <moisesbr@gmail.com>
+ */
+$lang['404page']               = 'Paĝo montrita okaze de eraro 404 (paĝo ne trovita)';
