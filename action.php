4f173a43dacaeab1f9af8f3b9731cd11f9d44c3f
Andreas Gohr <andi@splitbrain.org>
2025-09-25 15:32:31 +0200

    some cleanup for modern dokuwiki versions

--- action.php
+++ action.php
@@ -1,21 +1,34 @@
 <?php
-if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
+
+use dokuwiki\Extension\ActionPlugin;
+use dokuwiki\Extension\EventHandler;
+use dokuwiki\Extension\Event;
 
 
-class action_plugin_notfound extends DokuWiki_Action_Plugin {
-
-    function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('ACTION_ACT_PREPROCESS','BEFORE', $this, '_check404');
-        $controller->register_hook('TPL_CONTENT_DISPLAY','BEFORE', $this, '_show404');
+class action_plugin_notfound extends ActionPlugin
+{
+    /** @inheritdoc */
+    public function register(EventHandler $controller)
+    {
+        $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'check404');
+        $controller->register_hook('TPL_CONTENT_DISPLAY', 'BEFORE', $this, 'show404');
     }
 
-
-    function _check404(Doku_Event $event , $param) {
-        if($event->data != 'show') return false;
+    /**
+     * Event handler for ACTION_ACT_PREPROCESS
+     *
+     * Check if the requested page exists, if not change the action to 'notfound'
+     *
+     * @see https://www.dokuwiki.org/devel:events:ACTION_ACT_PREPROCESS
+     * @param Event $event Event object
+     * @param mixed $param optional parameter passed when event was registered
+     * @return bool true if the event was handled, false to let other handlers process it
+     */
+    public function check404(Event $event, $param)
+    {
+        if ($event->data != 'show') return false;
         global $INFO;
-        if($INFO['exists']) return false;
+        if ($INFO['exists']) return false;
 
         $event->data = 'notfound';
         $event->stopPropagation();
@@ -23,18 +36,29 @@ class action_plugin_notfound extends DokuWiki_Action_Plugin {
         return true;
     }
 
-    function _show404(Doku_Event $event, $param) {
+    /**
+     * Event handler for TPL_CONTENT_DISPLAY
+     *
+     * If the action is 'notfound', display the configured 404 page instead
+     *
+     * @see https://www.dokuwiki.org/devel:events:TPL_CONTENT_DISPLAY
+     * @param Event $event Event object
+     * @param mixed $param optional parameter passed when event was registered
+     * @return bool true if the event was handled, false to let other handlers process it
+     */
+    public function show404(Event $event, $param)
+    {
         global $ACT;
-        if($ACT != 'notfound') return false;
+        if ($ACT != 'notfound') return false;
         $event->stopPropagation();
         $event->preventDefault();
 
         global $ID;
         $oldid = $ID;
         $ID = $this->getConf('404page');
-        echo p_wiki_xhtml($ID,'',false);
+        echo p_wiki_xhtml($ID, '', false);
         $ID = $oldid;
-        $ACT='show';
+        $ACT = 'show';
 
         return true;
     }
